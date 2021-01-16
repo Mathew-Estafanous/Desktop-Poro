c5d32a341cd5290332f6414be64b6aff25c8f115
Ethan118 <ethan@tower10labs.com>
2021-01-16 16:05:09 -0500

    added another poro

--- src/main/java/com/javacados/Poro.java
+++ src/main/java/com/javacados/Poro.java
@@ -4,5 +4,24 @@ public class Poro {
     private int food;
     private int size;
 
+    public Poro(int food, int size) {
+        this.food = food;
+        this.size = size;
+    }
 
+    public int getFood() {
+        return food;
+    }
+
+    public void grow(int food) {
+        this.food += food;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
 }
