40cd8b95685d46f334d7518e0df98b88d3c8a3c6
Mathew-Estafanous <56979977+Mathew-Estafanous@users.noreply.github.com>
2021-01-16 11:40:12 -0500

    Add PORO GIF!!

--- src/main/java/com/javacados/MainApp.java
+++ src/main/java/com/javacados/MainApp.java
@@ -13,7 +13,11 @@ public class MainApp extends JFrame {
 
         setBackground(new Color(0, 0, 0, 0));
 
+        Icon imgIcon = new ImageIcon(this.getClass().getResource("/static/poro.gif"));
+        JLabel label = new JLabel(imgIcon);
+        label.setBounds(668, 43, 200, 200);
         JPanel drawPanel = new DrawPanel();
+        add(label);
         add(drawPanel);
     }
 }
