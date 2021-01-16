d6a4b8170e502defa8cf7ffa311b550beb1c8402
Ethan118 <ethan@tower10labs.com>
2021-01-16 14:07:50 -0500

    PORO IS HERE

--- src/main/java/com/javacados/MainFrame.java
+++ src/main/java/com/javacados/MainFrame.java
@@ -1,12 +1,26 @@
 package com.javacados;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
 
 public class MainFrame extends JFrame {
 
     public MainFrame() {
-        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setSize(100, 100);
+        Rectangle rect = GraphicsEnvironment
+                .getLocalGraphicsEnvironment()
+                .getDefaultScreenDevice()
+                .getDefaultConfiguration()
+                .getBounds();
+
+        setLocation((int) rect.getMaxX() - this.getWidth() - 80,
+                    (int) rect.getMaxY() - this.getHeight() - 80);
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setUndecorated(true);
         setVisible(true);
@@ -16,8 +30,9 @@ public class MainFrame extends JFrame {
 
         //setType(Type.UTILITY);
 
-        Icon imgIcon = new ImageIcon(this.getClass().getResource("/static/Poro.gif"));
-        JLabel label = new JLabel(imgIcon);
+        ImageIcon poroGif = new ImageIcon(this.getClass().getResource("/static/Poro.gif"));
+        poroGif.setImage(poroGif.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT));
+        JLabel label = new JLabel(poroGif);
         label.setBounds(668, 43, 500, 476);
         add(label);
     }
